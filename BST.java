5fc177805f7a5c0f0679f5532455496d4785ed06
Paul Ke <paulke29@yahoo.com>
2019-10-24 11:03:13 -0700

    Done

--- BST.java
+++ BST.java
@@ -1,2 +1,122 @@
-public class BST {
+
+
+class BST <T>{
+
+    private BinaryTreeNode root;
+
+    // initial TreeNode in the constructor
+    public BST() {
+        root = null;
+    }
+   // insert node into tree
+    public void insert(Comparable k){
+        //checking whether node is null or null;
+        if (root == null) {
+            root = new BinaryTreeNode(k, null, null);
+        }
+        else {
+            insert(root, k);
+        }
+    }
+
+    // delte node from BST
+    public void delete(Comparable k) {
+        root = delete(root, k);
+    }
+
+    //Finding the node from BST
+    public boolean find(Comparable k) {
+        return find(root, k);
+    }
+    //print out the whole BST
+    public void print() {
+        print(root);
+    }
+
+    private boolean find(BinaryTreeNode Tree, Comparable k) {
+        if (Tree == null)
+            return false;
+        if (Tree.getValue().equals(k))
+            return true;
+        if (k.compareTo(Tree.getValue()) < 0) {
+            return find(Tree.getLeft(), k);
+        }
+        else {
+            return find(Tree.getRight(), k);
+        }
+    }
+
+
+
+    private void insert(BinaryTreeNode T, Comparable k){
+        //delte the equal value
+        if (T.getValue().equals(k)){
+            delete(k);
+        }
+        // comparing value
+        if (k.compareTo(T.getValue()) < 0) {
+            if (T.getLeft() == null) {
+                T.setLeft(new BinaryTreeNode(k, null, null));
+            }
+            else {
+                insert(T.getLeft(), k);
+            }
+        }
+        else {
+
+            if (T.getRight() == null)
+                T.setRight( new BinaryTreeNode(k, null, null) );
+            else
+                insert(T.getRight(), k);
+        }
+    }
+    //print out all the node in the binary tree
+    private void print(BinaryTreeNode T) {
+        if(T!= null){
+            print(T.getLeft());
+            System.out.println(T.getValue());
+            print(T.getRight());
+        }
+    }
+
+    // finding the smallest Node in the BinaryTreeNode
+    private BinaryTreeNode smallestNode(BinaryTreeNode T)
+    {
+        if (T.getLeft() == null)
+            return T;
+        else
+            return smallestNode(T.getLeft());
+    }
+
+
+    // following the condition to delete the Node
+    private BinaryTreeNode delete(BinaryTreeNode T, Comparable k) {
+        if (T == null)
+            return null;
+        if (k.equals(T.getValue())) {
+
+            if (T.getLeft() == null && T.getRight() == null)
+                return null;
+            if (T.getLeft() == null)
+                return T.getRight();
+            if (T.getRight() == null)
+                return T.getLeft();
+
+//            BinaryTreeNode tmp = smallestNode(T.getRight());
+//            T.setValue( tmp.getValue() );
+//            T.setRight( delete(T.getRight(), tmp.getValue()) );
+            return T;
+
+        }
+        else if (k.compareTo(T.getValue()) < 0) {
+            T.setLeft( delete(T.getLeft(), k) );
+            return T;
+        }
+        else {
+            T.setRight( delete(T.getRight(), k) );
+            return T;
+        }
+    }
+
+
 }
